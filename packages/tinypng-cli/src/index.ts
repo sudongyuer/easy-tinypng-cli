c2f35b5886051cf783e371d75ce12c9eb172d504
Frozen FIsh <976499226@qq.com>
2022-07-30 14:10:48 +0800

    chore: update index.ts

--- packages/tinypng-cli/src/index.ts
+++ packages/tinypng-cli/src/index.ts
@@ -1,9 +1,6 @@
-import path from 'path'
-import { cwd } from 'node:process'
-import tinify from 'tinify'
 import { loadConfig } from 'unconfig'
-import chokidar from 'chokidar'
-import type { Config, ExportConfig } from './types'
+import type { ExportConfig } from './types'
+import { getAllConfigs, startOptimize } from './utils'
 const { config } = await loadConfig<ExportConfig>({
   sources: [
     {
@@ -15,25 +12,5 @@ const { config } = await loadConfig<ExportConfig>({
 // 获取config
 const configs = getAllConfigs(config)
 // 监听targetDir
-// 记录日志
-// tinify.key = 'xfxl7mvvzR3kvT8bDrQQJrDJMF1sW3wJ'
-// tinify.fromFile('./src/react.png').toFile('./src/react.png')
+startOptimize(configs)
 
-function getAllConfigs(config: ExportConfig) {
-  return config.configs
-}
-
-function startOptimize(configs: Config[]) {
-// 遍历配置获取targetDir
-  for (let i = 0; i < configs.length; i++) {
-    const { targetDir } = configs[i]
-    chokidar.watch(path.resolve(cwd(), targetDir), {
-      ignoreInitial: true,
-      atomic: true,
-      followSymlinks: true,
-    }).on('all', (event, pathDir) => {
-      // 新增时进行压缩TODO
-      // 删除时TODO
-    })
-  }
-}
