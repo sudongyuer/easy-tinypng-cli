6c649ca9edf66ffab69db7f16ec7ba1e30b42858
Frozen FIsh <976499226@qq.com>
2022-07-30 18:42:53 +0800

    chore: update index.ts

---
+++ packages/easy-tinypng-cli/src/index.ts
@@ -0,0 +1,16 @@
+import { loadConfig } from 'unconfig'
+import type { ExportConfig } from './types'
+import { getAllConfigs, startOptimize } from './utils.js'
+const { config } = await loadConfig<ExportConfig>({
+  sources: [
+    {
+      files: 'tiny.config',
+      extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json', ''],
+    },
+  ],
+})
+// 获取config
+const configs = getAllConfigs(config)
+// 监听targetDir
+startOptimize(configs)
+
